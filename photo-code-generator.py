37331a339d6481da54c5151e3188e401cfb0a79b
GrimReaper02 <fernando.garcia270202@gmail.com>
2024-03-17 13:57:00 -0600

    MonsterJam

--- photo-code-generator.py
+++ photo-code-generator.py
@@ -2,11 +2,11 @@
 import os
  
 # Get the list of all files and directories
-path = "C://Users//Fernando Garcia//Documents//fernando-garcia-cortez.github.io//Photography//231021-McCalls"
+path = "C://Users//Fernando Garcia//Documents//fernando-garcia-cortez.github.io//Photography//240307-MonsterJam"
 dir_list = os.listdir(path)
 
 # prints all files
 print(dir_list)
 for i in dir_list:
-    print("<img src=\"231021-McCalls/"+i+"\" class=\"centerImg\" style=\"width: 75%\">")
+    print("<img src=\"240307-MonsterJam/"+i+"\" class=\"centerImg\" style=\"width: 75%\">")
     print("<br>")
