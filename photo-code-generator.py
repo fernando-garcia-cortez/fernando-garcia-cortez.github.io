0352e52845ac4173003d5f9387614fd0da96db70
GrimReaper02 <fernando.garcia270202@gmail.com>
2024-06-24 22:17:28 -0600

    A

--- photo-code-generator.py
+++ photo-code-generator.py
@@ -2,11 +2,11 @@
 import os
  
 # Get the list of all files and directories
-path = "C://Users//Fernando Garcia//Documents//fernando-garcia-cortez.github.io//Photography//240428-LosAlamos"
+path = "C://Users//Fernando Garcia//Documents//fernando-garcia-cortez.github.io//Photography//240622-SandiaCrest"
 dir_list = os.listdir(path)
 
 # prints all files
 print(dir_list)
 for i in dir_list:
-    print("<img src=\"240428-LosAlamos/"+i+"\" class=\"centerImg\" style=\"width: 75%\">")
+    print("<img src=\"240622-SandiaCrest/"+i+"\" class=\"centerImg\" style=\"width: 75%\">")
     print("<br>")
